1a10a6b30f84960b25b49eb53cbaf308f9dfa5a3
Lauren Bell <laurengilbert0@gmail.com>
2023-03-13 16:46:26 +0000

    vnet added to main.tf file

--- main.tf
+++ main.tf
@@ -8,10 +8,31 @@ resource "azurerm_resource_group" "practice_resource_group" {
   location = ""
 }
 
+resource "azurerm_network_security_group" "practice_sg" {
+  name                = "practice_sg"
+  location            = azurerm_resource_group.practice_resource_group.location
+  resource_group_name = azurerm_resource_group.practice_resource_group.name
+}
+
+resource "azurerm_virtual_network" "practice_vnet" {
+  name                = "practice_vnet"
+  location            = azurerm_resource_group.practice_resource_group.location
+  resource_group_name = azurerm_resource_group.practice_resource_group.name
+  address_space       = ["10.0.0.0/16"]
+
+
+  subnet {
+    name           = "practice_subnet"
+    address_prefix = "10.0.1.0/24"
+  }
+
+
+}
+
 resource "azurerm_key_vault" "practice_keyvault" {
   name                        = "practice_keyvault"
-  location                    = azurerm_resource_group.example.location
-  resource_group_name         = azurerm_resource_group.example.name
+  location                    = azurerm_resource_group.practice_resource_group.location
+  resource_group_name         = azurerm_resource_group.practice_resource_group.name
   enabled_for_disk_encryption = true
   tenant_id                   = 
 
