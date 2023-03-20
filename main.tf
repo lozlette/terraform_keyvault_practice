ac8355005120bff92eb92af94256d5a8d889116d
Lauren Bell <laurengilbert0@gmail.com>
2023-03-20 10:23:03 +0000

    ad user added

--- main.tf
+++ main.tf
@@ -17,6 +17,10 @@ provider "azurerm" {
   }
 }
 
+resource "azuread_user" "ad_user" {
+  user_principal_name = var.user_principal_name
+  display_name        = var.display_name
+}
 
 resource "azurerm_resource_group" "practice_resource_group" {
   name     = "practice_resource_group"
