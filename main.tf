d586b041dc40709b61a85c6dcb2dcbe2c4d5d5e8
Lauren Bell <laurengilbert0@gmail.com>
2023-03-20 15:09:11 +0000

    security group rules added for ssh access to vm

--- main.tf
+++ main.tf
@@ -31,6 +31,27 @@ resource "azurerm_network_security_group" "practice_sg" {
   name                = "practice_sg"
   location            = azurerm_resource_group.practice_resource_group.location
   resource_group_name = azurerm_resource_group.practice_resource_group.name
+    security_rule {
+        name                       = "ssh_inbound"
+        priority                   = 100
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "*"
+        source_port_range          = "22"
+        destination_port_range     = "22"
+    }
+
+    security_rule {
+        name                       = "ssh_outbound"
+        priority                   = 100
+        direction                  = "Outbound"
+        access                     = "Allow"
+        protocol                   = "*"
+        source_port_range          = "22"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
 }
 
 # vnet 
